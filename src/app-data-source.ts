812ebc929cb698d13bea326efea30d484af387e2
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-06-17 21:46:00 +0200

    migrations

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,7 +1,4 @@
 import { DataSource } from "typeorm";
-import { God } from "./entity/God.entity";
-import { Partners } from "./entity/Partners.entity";
-import { Child } from "./entity/Child.entity";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
@@ -10,7 +7,8 @@ export const AppDataSource = new DataSource({
   username: "root",
   password: "root",
   database: "greek-tree",
-  entities: [God, Partners, Child],
+  entities: ["build/**/*entity.{ts,js}"],
+  migrations: ["build/migrations/*.{ts,js}"],
   logging: false,
   synchronize: true,
 });
